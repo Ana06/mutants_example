5c53923f4bf11f02671a2cd7691884dc60279fbc
Ana María Martínez Gómez <ammartinez@suse.de>
2017-06-29 14:57:34 +0200

    Better tests

--- spec/mutants_example/smoking_spec.rb
+++ spec/mutants_example/smoking_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe MutantsExample::Smoking do
   it "allow pepe to smoke" do
     expect(MutantsExample::Smoking.can_smoke?(adult[:age])).to be true
   end
+
+  context "better tests" do
+    (0..17).each do |age|
+      it "don't allow children of #{age} years old to smoke" do
+        expect(MutantsExample::Smoking.can_smoke?(age)).to be false
+      end
+    end
+
+    (18..100).each do |age|
+      it "allow adults of #{age} years old to smoke" do
+        expect(MutantsExample::Smoking.can_smoke?(age)).to be true
+      end
+    end
+  end
 end
